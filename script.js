a52f1bf4dd8312bc55185bb34854cc798439c646
Mon981 <alwaysblue.marketing@gmail.com>
2024-10-08 14:11:31 +0200

    The page does not reload after pressing enter

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let buttonEL = document.getElementById("btn")
-buttonEL.innerHTML = "Generate Secure Password"
+buttonEL.textContent = "Generate Secure Password"
 let passText = document.getElementById("pass-text")
 
 let inputEl = document.getElementById("input")
 
 buttonEL.addEventListener("click", generatePass)
+inputEl.addEventListener("keypress", function(e){
+    if (e.key === "Enter"){
+        generatePass()
+    }
+})
 
 function generatePass(){
     let numOfChar = parseInt(inputEl.value)
     if (isNaN(numOfChar) || numOfChar <= 0 || numOfChar > 25 || numOfChar < 9){
         passText.textContent = "The number of characters must be a number between 9 and 25."
+        
     } else {
         const pass = new Uint32Array(numOfChar)
         crypto.getRandomValues(pass)    
@@ -19,6 +25,9 @@ function generatePass(){
             
         }
         passText.textContent = finalPass
+        
+        
     }
+    inputEl.value=""
 
 }
