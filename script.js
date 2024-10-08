eadaa563bee6463fc034ed781d9e0e9e547d7c79
Mon981 <alwaysblue.marketing@gmail.com>
2024-10-08 11:01:57 +0200

    A conditional has been added to check that the entered value is valid.

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ let buttonEL = document.getElementById("btn")
 buttonEL.innerHTML = "Generate Secure Password"
 let passText = document.getElementById("pass-text")
 
+let inputEl = document.getElementById("input")
 
 buttonEL.addEventListener("click", generatePass)
 
 function generatePass(){
-    let inputEl = document.getElementById("input")
     let numOfChar = parseInt(inputEl.value)
-    const pass = new Uint32Array(numOfChar)
-    crypto.getRandomValues(pass)    
-    let finalPass = ""
-    for (let i = 0; i < pass.length; i++){
-        finalPass += String.fromCharCode(pass[i]%93 + 33)
-        
+    if (isNaN(numOfChar) || numOfChar <= 0 || numOfChar > 25 || numOfChar < 9){
+        passText.textContent = "The number of characters must be a number between 9 and 25."
+    } else {
+        const pass = new Uint32Array(numOfChar)
+        crypto.getRandomValues(pass)    
+        let finalPass = ""
+        for (let i = 0; i < pass.length; i++){
+            finalPass += String.fromCharCode(pass[i]%93 + 33)
+            
+        }
+        passText.textContent = finalPass
     }
-    passText.textContent = finalPass
 
 }
